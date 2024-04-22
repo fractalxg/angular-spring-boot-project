e902fd06f3582d6c9a70dbce415044e899520985
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-04-22 18:53:09 -0300

    initial commit

---
+++ backend/src/main/java/com/example/angularspringbootproject/AngularSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.angularspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/angularspringbootproject/AngularSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.angularspringbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
