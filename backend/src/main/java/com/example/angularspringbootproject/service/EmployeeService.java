add98450a1754d54b1b61a6df68c0b263613c151
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-04-25 19:27:07 -0300

    feat: create and insert employee table

--- backend/src/main/java/com/example/angularspringbootproject/service/EmployeeService.java
+++ backend/src/main/java/com/example/angularspringbootproject/service/EmployeeService.java
@@ -1,7 +1,17 @@
 package com.example.angularspringbootproject.service;
 
+import com.example.angularspringbootproject.db.EmployeeDb;
+import com.example.angularspringbootproject.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeDb employeeDb;
+
+    public Employee saveEmployee(Employee employee){
+        return employeeDb.save(employee);
+    }
 }
