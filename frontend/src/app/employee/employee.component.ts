ce8fcac1d4c728ff5a4ce9187de71723680d6cc2
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-05-06 21:57:11 -0300

    feat: employee interface

--- frontend/src/app/employee/employee.component.ts
+++ frontend/src/app/employee/employee.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee.model';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit{
+  
+  employee: Employee = {
+    employeeId: 0,
+    employeeName: '',
+    employeeContactNumber: '',
+    employeeAddress: '',
+    employeeContactGender: '',
+    employeeContactDepartment: '',
+    employeeContactSkills: ''
+  }
 
+  constructor() {
+
+  }
+  ngOnInit(): void {
+  }
 }
---
+++ frontend/src/app/employee/employee.model.ts
@@ -0,0 +1,9 @@
+export interface Employee {
+    employeeId: number,
+    employeeName: string,
+    employeeContactNumber: string,
+    employeeAddress: string,
+    employeeContactGender: string,
+    employeeContactDepartment: string,
+    employeeContactSkills: string,
+}
