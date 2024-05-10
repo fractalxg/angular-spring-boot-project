ad513271d41f64df1a77b872efc36328473f2278
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-05-10 20:29:37 -0300

    feat: employee service get request

--- frontend/src/app/employee-list/employee-list.component.ts
+++ frontend/src/app/employee-list/employee-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee/employee.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrls: ['./employee-list.component.css']
+  styleUrls: ['./employee-list.component.css'],
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
+  constructor(private employeeService: EmployeeService) {
+    this.getEmployeeList();
+  }
+  ngOnInit(): void {}
 
+  getEmployeeList(): void {
+    this.employeeService.getEmployees().subscribe({
+      next: (res: Employee[]) => {
+        console.log(res);
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+      },
+    });
+  }
 }
