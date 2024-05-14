3679e6b09369eb6c6ad479cccbf4600dcb324656
Gabriel Machado <gabriel.alcantaramachadoz@gmail.com>
2024-05-14 18:42:09 -0300

    feat: delete employee method

--- frontend/src/app/employee-list/employee-list.component.ts
+++ frontend/src/app/employee-list/employee-list.component.ts
@@ -9,7 +9,6 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./employee-list.component.css'],
 })
 export class EmployeeListComponent implements OnInit {
-
   dataSource: Employee[] = [];
 
   displayedColumns: string[] = [
@@ -20,13 +19,24 @@ export class EmployeeListComponent implements OnInit {
     'employeeDepartment',
     'employeeGender',
     'employeeSkills',
-    'delete'
+    'delete',
   ];
 
   constructor(private employeeService: EmployeeService) {
     this.getEmployeeList();
   }
   ngOnInit(): void {}
+
+  deleteEmployee(employeeId: number): void {
+    this.employeeService.deleteEmployee(employeeId).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+      },
+    });
+  }
 
   getEmployeeList(): void {
     this.employeeService.getEmployees().subscribe({
